0a3e368b3dc0843031bf17d8db826f0a5854d676
Jnewton85 <jasen@jasentech.com>
2015-02-28 12:54:15 -0600

    initial commit

---
+++ app/controllers/detail.js
@@ -0,0 +1,15 @@
+
+var args = arguments[0] || {};
+
+
+$.closeBtn.addEventListener("click", function(){
+	$.detailWindow.close();
+});
+
+
+$.car.set(args.data);
+
+
+$.detailWindow.addEventListener("close",function(){
+	$.destroy();
+});
